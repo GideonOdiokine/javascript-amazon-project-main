d10c8ca6955bee3a300249d7b8d28ba9338abf93
GideonOdiokine <gidysmartodiokineh@gmail.com>
2025-02-11 01:56:49 +0100

    deliveryOptions

---
+++ data/deliveryOptions.js
@@ -0,0 +1,17 @@
+export const deliveryOptions = [
+  {
+    id: "1",
+    deliveryPrice: 0,
+    deliveryDate: 7,
+  },
+  {
+    id: "2",
+    deliveryPrice: 499,
+    deliveryDate: 3,
+  },
+  {
+    id: "3",
+    deliveryPrice: 999,
+    deliveryDate: 1,
+  },
+];
